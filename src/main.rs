d496cac9c76d30d53037d8f4500be4e6fb0cc1f4
subrat gupta <guptasubratkumar@gmail.com>
2022-12-04 21:03:57 +0530

    2

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,23 @@
-use image::{DynamicImage, GenericImageView};
-use reqwest::{self, blocking, Url};
+#[derive(Debug)]
+enum NAMES {
+    SUBRAT,
+    KALLI,
+}
+
+impl std::fmt::Display for NAMES {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 
 fn main() {
-    // Use the open function to load an image from a Path.
-    // `open` returns a `DynamicImage` on success.
-    let img_bytes = reqwest::blocking::get(
-        "https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx136800-WUh8ZeFNqzOv.jpg",
-    )
-    .unwrap()
-    .bytes()
-    .unwrap();
-    let img = image::load_from_memory(&img_bytes).unwrap();
-    // let img = getimg(
-    //     "https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx136800-WUh8ZeFNqzOv.jpg",
-    // );
-
-    // // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", img.dimensions());
-
-    // // The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
-
-    // Write the contents of this image to the Writer in PNG format.
-    img.save("test.png").unwrap();
-
-    // fn a(name: &str, number: u64) {
-    //     println!("{number}");
-    //
-    // }
-    // println!("{}", img_bytes);
-
-    // const NUM: u64 = 40;
-    // const NAME: &str = "SubratGupta";
-    // a(NAME, NUM);
+    let name1 = NAMES::SUBRAT;
+    let name2 = NAMES::KALLI;
+    println!(
+        "
+    {} 
+    {}
+    ",
+        name1, name2
+    );
 }
